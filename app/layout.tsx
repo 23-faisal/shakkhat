9ff6384af44083fdc88cc5d2056c6de68d0fa9fb
23-faisal <a.faisal23r@gmail.com>
2024-11-27 14:58:20 +0600

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  variable: "--font-plus-jakarta-sans",
+  display: "swap",
+  weight: ["300", "400", "500", "600", "700", "800"],
+});
+
+export const metadata: Metadata = {
+  title: "Shakkhat",
+  description: "A healthcare management system",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${plusJakartaSans.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
